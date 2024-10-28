f99bbc065acb538424a01c80b133f2c56829435c
Joseph <josephvincentlimbaroc@gmail.com>
2024-10-28 19:42:18 +0800

    updated nuxt config and .env.example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      appName: process.env.APP_NAME,
+      apiBaseURL: process.env.API_BASE_URL,
+    },
+  },
 })
